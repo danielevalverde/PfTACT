3ba0e1fbe1648bf924c6f2967e2f4cc3ceb94749
danielevalverde <danierzevalverde@gmail.com>
2024-01-03 09:37:23 -0300

    add 'and' 'or' logic for all output formats

--- output_formatter.py
+++ output_formatter.py
@@ -27,11 +27,16 @@ class OutputFormatter:
             output_file = "output.txt"
             with open(output_file, 'w') as out_file:
                 for file, keywords_found in results.items():
-                    out_file.write(f"Arquivo: {file}\n")
-                    if keywords_found:
-                        out_file.write(f"Palavras-chave encontradas: {', '.join(keywords_found)}\n\n")
+                    if operator == "e":
+                        if len(keywords_found) < qtd_strings:
+                            out_file.write(f"Arquivo: {file}\n")
+                            out_file.write("Busca não satisfeita.\n\n")
                     else:
-                        out_file.write("Nenhuma palavra-chave encontrada.\n\n")
+                        out_file.write(f"Arquivo: {file}\n")
+                        if keywords_found:
+                            out_file.write(f"Palavras-chave encontradas: {', '.join(keywords_found)}\n\n")
+                        else:
+                            out_file.write("Nenhuma palavra-chave encontrada.\n\n")
             print(f"Os resultados foram salvos em '{output_file}'.")
         elif output_format == "3":
             csv_output_file = "output.csv"
@@ -39,10 +44,14 @@ class OutputFormatter:
                 csv_writer = csv.writer(csv_out_file)
                 csv_writer.writerow(["Arquivo", "Palavras-chave Encontradas"])
                 for file, keywords_found in results.items():
-                    if keywords_found:
-                        csv_writer.writerow([file, ', '.join(keywords_found)])
+                    if operator == "e":
+                        if len(keywords_found) < qtd_strings:
+                            csv_writer.writerow([file, "Busca não satisfeita"])
                     else:
-                        csv_writer.writerow([file, "Nenhuma palavra-chave encontrada"])
+                        if keywords_found:
+                            csv_writer.writerow([file, ', '.join(keywords_found)])
+                        else:
+                            csv_writer.writerow([file, "Nenhuma palavra-chave encontrada"])
             print(f"Os resultados foram salvos em '{csv_output_file}' (CSV).")
         elif output_format == "4":
             pdf_output_file = "output.pdf"
@@ -53,12 +62,16 @@ class OutputFormatter:
             for file, keywords_found in results.items():
                 c.drawString(100, y_position, f"Arquivo: {file}")
                 y_position -= 15
-                if keywords_found:
-                    c.drawString(100, y_position, f"Palavras-chave encontradas: {', '.join(keywords_found)}")
+                if operator == "e":
+                    if len(keywords_found) < qtd_strings:
+                        c.drawString(100, y_position, f"Busca não satisfeita:")
                 else:
-                    c.drawString(100, y_position, "Nenhuma palavra-chave encontrada.")
+                    if keywords_found:
+                        c.drawString(100, y_position, f"Palavras-chave encontradas: {', '.join(keywords_found)}")
+                    else:
+                        c.drawString(100, y_position, "Nenhuma palavra-chave encontrada.")
                 y_position -= 15
-                y_position -= 15  # Espaço adicional entre os resultados
+                y_position -= 15
             c.save()
             with open(pdf_output_file, 'wb') as pdf_out_file:
                 pdf_out_file.write(buffer.getvalue())
